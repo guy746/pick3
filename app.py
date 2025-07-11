9d63d1b3e76b6418b9238877557f370bf5a167a3
guy746 <guy746@gmail.com>
2025-07-11 16:46:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def handle_clear_missed_alert():
 
 if __name__ == '__main__':
     start_background_threads()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), allow_unsafe_werkzeug=True)
